20db324faf6dc7230ad81ae0031e80f3ac7c3022
anandviswam <anandv2028@cs.sjcetpalai.ac.in>
2024-10-01 14:55:26 +0530

    Add files via upload
    my first python

---
+++ student_details.py
@@ -0,0 +1,12 @@
+print("hello python")
+name=input("enter your name:")
+print("mame of student",name)
+roll_number=input("your rolll number:")
+print("roll number of the students",roll_number)
+cgpa=float(input("enter your cgpa"))
+percentage_mark=cgpa*10
+print("name of the student:")
+print()
+
+
+
